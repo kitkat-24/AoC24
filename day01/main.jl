7a3cc20dc307e4e97461a8c958dab33e8d97fa59
Katerina <kinmersha@gmail.com>
2024-12-01 16:28:37 -0500

    p2 done

--- day01/main.jl
+++ day01/main.jl
@@ -1,7 +1,7 @@
 # Read in data
 left = Vector{Int32}();
 right = Vector{Int32}();
-for line in eachline("test.txt")
+for line in eachline("input.txt")
     nums = split(line)
     append!(left, parse(Int32, nums[1]))
     append!(right, parse(Int32, nums[2]))
@@ -16,3 +16,12 @@ for (v1, v2) in zip(left,right)
     append!(distances, abs(v2 - v1))
 end
 dist = sum(distances)
+
+# Part two
+appearances = Dict{Int32, Int32}()
+for num in left
+    if !haskey(appearances, num)
+        appearances[num] = sum(num .== right)
+    end
+end
+similarity = sum(map(v -> v * appearances[v], left))
