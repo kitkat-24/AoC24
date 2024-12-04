a833c7beaf8aa6954c1f32c15bc7c01209fa7e00
Katerina <kinmersha@gmail.com>
2024-12-04 01:17:14 -0500

    Day 04 part 2!
    Again keeping the two parts separate for clarity even though the solution could be wrapped up into the same loop ofc. Not like this one takes long either way

--- day04/main.jl
+++ day04/main.jl
@@ -1,6 +1,5 @@
 # Read data and parse
 test1 = readlines("day04/test1.txt")
-# test2 = readlines("day04/test2.txt", String)
 lines = readlines("day04/input.txt")
 
 
@@ -47,9 +46,48 @@ function p1(text)
         end
     end
 
-    text[invalid] .= '.'
+    # text[invalid] .= '.'
 
     return count
 end
 
-p1(reduce(vcat, permutedims.(collect.(lines))))
+function checkX(text, i, corners)
+    c = 0
+    for cc in corners
+        if !checkbounds(Bool, text, i+cc) || !checkbounds(Bool, text, i-cc)
+            continue
+        end
+        if text[i+cc] == 'M' && text[i-cc] == 'S'
+            c += 1
+        end
+    end
+    return c == 2
+end
+
+function p2(text)
+    invalid = trues(size(text))
+
+    # Construct diagonal direction offset indices
+    adj = [CartesianIndex(m1,m2) for m2 = -1:2:1 for m1 = -1:2:1]
+
+    count = 0
+    for i ∈ eachindex(IndexCartesian(), text)
+        if text[i] == 'A' # Every X-MAS needs its A!
+            if checkX(text, i, adj)
+                i2 = [i+dir for dir in adj]
+                invalid[i2] .= false
+                invalid[i] = false
+                count += 1
+            end
+        end
+    end
+
+    # text[invalid] .= '.'
+    # testPrint(text)
+
+    return count
+end
+
+text = reduce(vcat, permutedims.(collect.(lines)))
+p1(text)
+p2(text)
