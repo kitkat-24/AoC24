6f068d19a04421f4ba49031e648b305cc851f2aa
Katerina <kinmersha@gmail.com>
2024-12-07 01:08:01 -0500

    Did some day06 optimizations
    Cut memory allocation by like... two orders of magnitude, similar-ish reduction in run-time. Big lesson was to 1) not do needless copying leftover from p1 and 2) structs are evil ig? (at least mutable ones...)

--- day06/main.jl
+++ day06/main.jl
@@ -1,65 +1,16 @@
 using Printf
 # Read data and parse
-test = stack(readlines("day06/test.txt"); dims=1);
+# test = stack(readlines("day06/test.txt"); dims=1);
 grid = stack(readlines("day06/input.txt"); dims=1);
 
-mutable struct Guard
-    idx
-    const states
-    const moves 
 
-    # Inner constructor for constants
-    function Guard(i)
-        new(i, "^>v<", [CartesianIndex(-1,0), CartesianIndex(0,1), CartesianIndex(1,0), CartesianIndex(0,-1)])
-    end
-end
+turn(guardState) = guardState = guardState % 4 + 1
+# move(guard::Guard, idx::CartesianIndex) = idx = idx + guardMoves[guard.idx]
+# marker(guard::Guard) = (guard.idx == 1 || guard.idx == 3) ? '|' : '-'
+# vert(guard::Guard) = guard.idx % 2 == 1
 
-function turn!(guard)
-    guard.idx = guard.idx % length(guard.states) + 1
-end
-
-function setIdx!(guard, state)
-    guard.idx = findfirst(isequal(state), guard.states)
-end
-
-function move(guard, idx::CartesianIndex)
-    idx = idx + guard.moves[guard.idx]
-end
-
-function marker(guard::Guard)
-    (guard.idx == 1 || guard.idx == 3) ? '|' : '-'
-end
-
-function walkPath(grid, findCycles)
-    path = copy(grid)
-    idx = findfirst(grid .== '^')
-    guard = Guard(1)
-    nextIdx = move(guard, idx)
-    path[idx] = marker(guard)
-    turns = Set()
-    while (checkbounds(Bool, grid, nextIdx))
-        if isObst(grid[nextIdx])
-            newTurn = (idx, guard.idx)
-            # If we make a turn we made before, we're in a cycle
-            if findCycles && newTurn ∈ turns
-                return path, true
-            end
-            push!(turns, (idx, guard.idx))
-            turn!(guard)
-            path[idx] = '+'
-        else
-            idx = nextIdx
-            # path[idx] = marker(guard)
-        end
-        # if path[idx] != '+'; path[idx] = guard.states[guard.idx]; end
-        if path[idx] != '+'; path[idx] = marker(guard); end
-        nextIdx = move(guard, idx)
-        # printMat(path)
-        # sleep(0.1)
-    end
-
-    return path, false
-end
+isObst(c) = c == '#' || c == 'O'
+inPath(c) = c != '.' && !isObst(c)
 
 function printMat(m)
     for i = axes(m,1)
@@ -70,32 +21,64 @@ function printMat(m)
     end
 end
 
-# Brute force! Fuckit!
+function walkPath(grid, startIdx, findCycles)
+    guardMoves = [CartesianIndex(-1,0), CartesianIndex(0,1), CartesianIndex(1,0), CartesianIndex(0,-1)]
+    path = [startIdx]
+    idx = startIdx
+    guardState = 1 # Start facing up
+    @inbounds nextIdx = idx + guardMoves[guardState]
+    turns = Set()
+    while (checkbounds(Bool, grid, nextIdx))
+        if isObst(grid[nextIdx])
+            newTurn = (idx, guardState)
+            # If we make a turn we made before, we're in a cycle
+            if findCycles && newTurn ∈ turns
+                return path, true
+            end
+            push!(turns, newTurn)
+            guardState = turn(guardState)
+        else
+            idx = nextIdx
+        end
+        if !findCycles
+            push!(path, idx)
+        end
+        @inbounds nextIdx = idx + guardMoves[guardState]
+    end
+
+    return path, false
+end
+
 function findObstructions(grid, path)
     obstaclesFound = 0
-    for idx ∈ eachindex(grid)
-        if grid[idx] == '#' || grid[idx] == '^'
+    g2 = copy(grid)
+    start = findfirst(grid .== '^')
+    for idx ∈ path
+        if grid[idx] == '^'
             continue
         end
-        g2 = copy(grid)
         g2[idx] = 'O'
-        # printMat(g2)
-        p2, obst = walkPath(g2, true)
-        # if obst
-        #     # printMat(p2)
-        # end
-        obstaclesFound += obst 
+        p2, obst = walkPath(g2, start, true)
+        obstaclesFound += obst
+        g2[idx] = '.' # Reset grid
     end
     return obstaclesFound
 end
 
-inPath(c) = c != '.' && c != '#'
-isObst(c) = c == '#' || c == 'O'
-
 # grid = test;
+
 # P1
-@time path, _ = walkPath(grid, false);
-printMat(path)
-@printf "P1: %d locations visited!\n" sum(inPath.(path))
+@time path, _ = walkPath(grid, findfirst(grid .== '^'), false);
+uniquePath = unique(path)
+# @profview path, _ = walkPath(grid, false)
+# printMat(path)
+@printf "P1: %d locations visited!\n" length(uniquePath)
+
 #P2
-@time numObst = findObstructions(grid, path)
+@time numObst = findObstructions(grid, uniquePath)
+@profview begin
+    for i = 1:10
+        numObst = findObstructions(grid, uniquePath)
+    end
+end
+@printf "P2: %d possible obstructions!\n" numObst
