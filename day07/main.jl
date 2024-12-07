6461b76e39ecd1149f2d4443c335257647518b3d
Katerina <kinmersha@gmail.com>
2024-12-07 14:20:19 -0500

    day07 p2
    Shoulda known I couldn't get away with doing it in pure binary. At least I have like no allocations!

--- day07/main.jl
+++ day07/main.jl
@@ -5,7 +5,6 @@ using Printf
 # numbers::SVector{SVector{Int}}. Now allocating 25 KiB. I don't get it man...
 
 test = readlines("day07/test.txt")
-# input = test;
 input = readlines("day07/input.txt")
 
 
@@ -16,39 +15,52 @@ function parseInput(line)
     return ans, nums
 end
 
-data = parseInput.(input)
-answers = [d[1] for d in data]
-numbers = [d[2] for d in data]
-
-function permToStr(perm, len)
+function permToStr(perm, base, len)
     ans = ""
     for i = 1:len
-        ans = (perm & 1 == 1 ? "*" : "+") * ans
-        perm >>= 1
+        if perm % base == 0
+            ans = "+" * ans
+        elseif perm % base == 1
+            ans = "*" * ans
+        else
+            ans = "||" * ans
+        end
+        perm /= base
     end
     return ans
 end
 
-function findTrueConstraints(answers, numbers)
+function operate(perm, base, acc, num)
+    if perm % base == 0
+        acc += num
+    elseif perm % base == 1
+        acc *= num
+    elseif perm % base == 2 # Concatenate numbers
+        acc = acc * 10^(ceil(Int, log10(num+1))) + num
+    else
+        error("uh oh!")
+    end
+end
+
+function findTrueConstraints(answers, numbers; base=2)
     solvable = Vector{Bool}()
     solutions = Vector{Int}()
     for idx ∈ eachindex(answers)
         nums = numbers[idx]
         nOp = length(nums) - 1
-        nPerms = 2 ^ nOp
+        nPerms = base ^ nOp
         # Encode operators as 0 => +, 1 => * so we can just count an integer
-        acc = 0
+        acc = nums[1]
         for perm = 0:nPerms-1
-            mask = nPerms >> 1
+            p2 = perm
             acc = nums[1]
             i = 2
             for _ = 1:nOp
-                if perm & mask == 0
-                    acc += nums[i]
-                else
-                    acc *= nums[i]
+                acc = operate(p2, base, acc, nums[i])
+                if acc > answers[idx]
+                    break
                 end
-                mask >>= 1
+                p2 ÷= base
                 i += 1
             end
 
@@ -66,12 +78,16 @@ function findTrueConstraints(answers, numbers)
     return solvable, solutions
 end
 
-function sumP1(answers, solvable)
-    sum(answers[solvable])
-end
 
+data = parseInput.(input)
+answers = [d[1] for d in data]
+numbers = [d[2] for d in data]
 
 @time solvable, solns = findTrueConstraints(answers, numbers)
-@printf "P1: Sum of true test values = %d" sumP1(answers, solvable)
+@printf "P1: Sum of true test values = %d\n" sum(answers[solvable])
 
+# Only look at ones not already solvable
+idx = findall(.!solvable)
+@time fixable, fixSolns = findTrueConstraints(answers[idx], numbers[idx], base=3)
+@printf "P2: Sum of true test values = %d\n" sum(answers[solvable]) + sum(answers[idx[fixable]])
 
