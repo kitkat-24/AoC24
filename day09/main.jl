bf46cebf75cf236cc7429f43e84cb8191f5eb6f6
Katerina <kinmersha@gmail.com>
2024-12-09 22:44:06 -0500

    day 09 p2
    Not first try :( My initial solution was super slow *and* wrong (was trying to use structs to represent mem blocks and have 5GB of allocations). Went back to p1 approach adpated like I'd originally imagined and got it working after a bit, optimized down to run decently fast (70ms), but surely there is some fancy DSA way to do this.

--- day09/main.jl
+++ day09/main.jl
@@ -22,9 +22,8 @@ function printMemMap(mem)
     println(join((getChar(c) for c ∈ mem)))
 end
 
-function solve!(mem)
+function solveP1!(mem)
     a = 1; b = length(mem)
-    # printMemMap(mem)
     while a < b
         if mem[a] >= 0
             a += 1
@@ -33,17 +32,73 @@ function solve!(mem)
         else # Have a free block at a and full one at b. Swap!
             mem[a] = mem[b]
             mem[b] = -1
-            # printMemMap(mem)
         end
     end
     return findlast(mem .>= 0)
 end
 
-computeP1(mem, endex) = sum(((i-1)*mem[i] for i ∈ 1:endex))
+# @views macro turns mem[...] slices into views (no allocation)
+@views function solveP2!(mem)
+    b = length(mem)
+    astart = 1 # Used to not re-examine full blocks (again saved like 40% runtime)
+    while b > 0
+        if mem[b] == -1
+            b -= 1
+        else
+            # Compute contiguous block size
+            b2 = b
+            while b2 - 1 > 0 && mem[b2-1] == mem[b]
+                b2 -= 1
+            end
+            n = b - b2 + 1
 
-@time begin
+            # Search for block to fill
+            a = astart
+            first = true
+            while a <= b - n
+                if mem[a] >= 0
+                    a += 1
+                    astart = first ? a : astart
+                    continue
+                end
+                first = false
+
+                # Nasty unrolled loop instead of all(mem[...]) to get rid of # allocations, saved ~40% runtime
+                contiguous = true
+                for a2 = a+1:a+n-1
+                    if mem[a2] != -1
+                        contiguous = false
+                        break
+                    end
+                end
+                if contiguous
+                    mem[a:a+n-1] .= mem[b2:b]
+                    mem[b2:b] .= -1
+                    break
+                end
+                a += n
+            end
+
+            b -= n # advance to next block
+        end
+    end
+end
+
+computeChecksum(mem) = sum(((i-1)*mem[i] for i ∈ eachindex(mem) if mem[i] >= 0))
+
+@time begin # P1
 data = input;
 mem = buildMemMap(data)
-endex = solve!(mem)
-@printf "P1: checksum = %d\n" computeP1(mem, endex)
+solveP1!(mem)
+@printf "P1: checksum = %d\n" computeChecksum(mem)
 end
+
+@time begin # P2
+data = input;
+mem = buildMemMap(data)
+# printMemMap(mem)
+solveP2!(mem)
+# printMemMap(mem)
+@printf "P2: checksum = %d\n" computeChecksum(mem)
+end
+
