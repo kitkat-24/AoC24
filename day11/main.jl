0868fa4c5f5d93e70403a3cf71e60d770a33b32e
Katerina <kinmersha@gmail.com>
2024-12-11 01:10:15 -0500

    day11 part 2
    Actually not bad after I stopped being stupid and started just recursively coutning stones spawned in the "lifetime" of each one instead of actually maintaining the list.
    
    Also using Dict instead of the default IDict provided way better @memoize performance

--- day11/main.jl
+++ day11/main.jl
@@ -2,23 +2,28 @@ using Memoization
 test = parse.(Int, split(read("day11/test.txt", String)))
 input = parse.(Int, split(read("day11/input.txt", String)))
 
-@memoize function blink(stone)
+# Just return # of stones spawned by a certain number of blinks for a given stone so we can process recursively
+@memoize Dict function blink(stone, depth)
+    if depth == 0
+        return 1
+    end
+
     if stone == 0
-        return [1]
-    elseif ceil(Int, log10(stone+1)) % 2 == 0
-        n = 10^(ceil(Int, log10(stone+1)) ÷ 2)
-        return [stone ÷ n, stone % n]
+        return blink(1, depth - 1)
     else
-        return [stone * 2024]
+        digits = ceil(Int, log10(stone+1))
+        if iseven(digits)
+            n = 10^(digits >> 1)
+            return blink(stone ÷ n, depth - 1) + blink(stone % n, depth - 1)
+        else
+            return blink(stone * 2024, depth - 1)
+        end
     end
 end
 
 @time begin
 stones = copy(input)
-nBlinks = 25
-for n = 1:nBlinks
-    i = 1
-    global stones = reduce(vcat, blink.(stones))
-end
-println("Numbers of stones after $(nBlinks) is $(length(stones))")
+nBlinks = 75
+nStones = sum(blink.(stones, nBlinks))
+println("Numbers of stones after $(nBlinks) blinks is $(nStones)")
 end
