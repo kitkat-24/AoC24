eb80ec6e0f0e0945779f9b38537a0ea8e14d3cb6
Katerina <kinmersha@gmail.com>
2024-12-13 02:14:06 -0500

    day12 p2
    This one was a pain. Had to get a hint from reddit about looking for corners bc my approach for walking the edge of the shape was failing off-by-1 a lot but I couldn't think of a good way to fix it. In hindsight, maybe could have fixed it by saving row/col + direction instead of looking for repeated corners

--- day12/main.jl
+++ day12/main.jl
@@ -1,8 +1,5 @@
 readInput(fname) = stack(readlines("day12/$(fname).txt"); dims=1)
 
-# Theory: assign every tile to a unique group, then merge until all tiles either
-# have no adjacent ones with the same value, or all neighbors are in the same
-# group
 function bfs!(groupMap, grid, i, g)
     adj = CartesianIndex.(((1,0), (0,1), (-1,0), (0,-1)))
     groupMap[i] = g
@@ -25,6 +22,8 @@ function bfs!(groupMap, grid, i, g)
     perim
 end
 
+# Approach: Start at an ungrouped tile, use BFS to flood-fill out the group,
+# counting the perimeter as we go
 function findGroups(grid)
     groupMap = zeros(UInt16, size(grid)) # Input is 140x140 so max 19600 unique groups
 
@@ -41,15 +40,79 @@ function findGroups(grid)
     groupMap, groups, perims
 end
 
-function p1Cost(groupMap, groups, perims)
-    areas = [sum(groupMap .== g) for g ∈ groups]
+function p1Cost(areas, perims)
     sum(areas .* perims)
+end
+
+# Given we already have groups, just walk along the edge of each group to count the sides
+function countSides(groups, gmap)
+    adj = CartesianIndex.(((1,0), (0,1), (-1,0), (0,-1)))
+    down, right, up, left = adj
+    sides = []
+    # "Corner kernel
+    ck = collect(CartesianIndices((2,2))) .- CartesianIndex(1,1)
+
+    # I am not dealing with literal edge cases rn...
+    x,y = size(gmap)
+    gmap2 = typemax(eltype(gmap)) .* ones(eltype(gmap), x+2,y+2)
+    gmap2[2:end-1,2:end-1] = gmap
+
+    val(x) = gmap2[x]
+
+    for g ∈ groups
+        idx = gmap2 .== g
+        if sum(idx) == 1
+            push!(sides, 4)
+            continue
+        end
+        same(x) = val(x) == g
+
+        corners = 0
+        for i ∈ findall(idx)
+            if !same(i + down) && !same(i + right) # bottom-right
+                corners += 1
+            end
+            if !same(i + right) && !same(i + up) # top-right
+                corners += 1
+            end
+            if !same(i + up) && !same(i + left) # top-left
+                corners += 1
+            end
+            if !same(i + left) && !same(i + down) # bottom-left
+                corners += 1
+            end
+            # Concave corners
+            if same(i+left) && same(i+up) && !same(i+left+up) # bottom-right
+                corners += 1
+            end
+            if same(i+down) && same(i+left) && !same(i+down+left) # top-right
+                corners += 1
+            end
+            if same(i+right) && same(i+down) && !same(i+right+down) # top-left
+                corners += 1
+            end
+            if same(i+up) && same(i+right) && !same(i+up+right) # bottom-left
+                corners += 1
+            end
+        end
+        push!(sides, corners)
+    end
+    sides
+end
+
+function p2Cost(areas, sides)
+    sum(areas .* sides)
 end
 
 @time begin
     file = "input"
     grid = readInput(file)
     gmap, groups, perims = findGroups(grid)
-    cost = p1Cost(gmap, groups, perims)
+    areas = [sum(gmap .== g) for g ∈ groups]
+    cost = p1Cost(areas, perims)
     println("P1 cost for $(file) = $(cost)")
+
+    sides = countSides(groups, gmap)
+    cost = p2Cost(areas, sides)
+    println("P2 cost for $(file) = $(cost)")
 end
