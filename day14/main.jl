107fc2cc2246632798f6406142d3456def0cee9f
Katerina <kinmersha@gmail.com>
2024-12-15 00:53:03 -0500

    day14 p2
    VScode seems to break the readline() function for input, but anyway the p2 search worked with a little hint from the subreddit about looking for clusters bc i honestly had no fucking idea what to even expect the endcase to be (and fuck looking thru 10,000 frames)

--- day14/main.jl
+++ day14/main.jl
@@ -22,48 +22,76 @@ function gridBounds(fn)
     end
 end
 
-function printGrid(xs, ys, bounds)
-    grid = ['.' for _ in 1:bounds[2], _ in 1:bounds[1]]
+function printGrid(pf, bounds)
+    grid = [['.' for _ in 1:bounds[1]] for _ in 1:bounds[2]]
     robots = counter(Pair)
-    for (x,y) in zip(xs,ys)
-        p = Pair(x,y)
-        robots[p] += 1
+    for p ∈ pf
+        p2 = Pair(p[1], p[2])
+        robots[p2] += 1
     end
     for (k,v) ∈ robots
-        grid[k[2]+1, k[1]+1] = Char('0' + v)
+        grid[k[2]+1][k[1]+1] = Char('0' + v)
     end
-    display(grid)
+    for line ∈ grid
+        println(String(line))
+    end
     grid
 end
 
-function simulate(robots, turns, bounds)
-    x = []; y = []
-    for robot ∈ robots
-        # Just do all turns at once
-        pf = robot.p + turns * robot.v
-        # Wrap back into grid
-        pf = pf .% bounds
-        # Wrap negatives
-        pf[1] = pf[1] < 0 ? pf[1] + bounds[1] : pf[1]
-        pf[2] = pf[2] < 0 ? pf[2] + bounds[2] : pf[2]
-        push!(x, pf[1])
-        push!(y, pf[2])
-    end
+function moveBot(p, v, t, bounds)
+    pf = p + t * v
+    # Wrap back into grid
+    pf = pf .% bounds
+    # Wrap negatives
+    pf[1] = pf[1] < 0 ? pf[1] + bounds[1] : pf[1]
+    pf[2] = pf[2] < 0 ? pf[2] + bounds[2] : pf[2]
+    pf
+end
 
-    # grid = printGrid(x,y,bounds)
+function simulate(robots, turns, bounds)
+    pf = map(bot -> moveBot(bot.p, bot.v, turns, bounds), robots)
+    x = [p[1] for p ∈ pf]
+    y = [p[2] for p ∈ pf]
 
     mid = floor.(Int, bounds ./ 2)
-    # grid[mid[2], :] .= ' '
-    # grid[:, mid[1]] .= ' '
-    # display(grid)
     q1 = sum((x .< mid[1]) .&& (y .< mid[2]))
     q2 = sum((x .> mid[1]) .&& (y .< mid[2]))
     q3 = sum((x .< mid[1]) .&& (y .> mid[2]))
     q4 = sum((x .> mid[1]) .&& (y .> mid[2]))
     safetyFactor = q1*q2*q3*q4
+    safetyFactor, (q1,q2,q3,q4), pf
+end
+
+function findXmasTree(robots, bounds)
+    t = 1
+    while t < 10000
+        _, quads, pf = simulate(robots, t, bounds)
+        for p ∈ pf
+            dist = 15
+            numNear = mapreduce(x -> sqrt((x[1]-p[1])^2 + (x[2]-p[2])^2) < dist, +, pf)
+            if numNear > 200
+                printGrid(pf, bounds)
+                println("Found tree (turns = $(t)? N to continue, Y to exit")
+                r = readline()
+                println("Read: $(r)")
+                @goto doneLoop
+                if r == 'N'
+                    break
+                end
+            end
+        end
+        t += 1
+        if t % 100 == 0
+            println("Turn $(t)/10000")
+        end
+    end
+    @label doneLoop
+    t
 end
 
 fn = "input"
 robots = readInput(fn)
 gridSize = gridBounds(fn)
 simulate(robots, 100, gridSize)
+
+findXmasTree(robots, gridSize)
