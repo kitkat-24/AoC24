da89b4f12dbfae52fdc5baf6299ccae0d6867b97
Katerina <kinmersha@gmail.com>
2025-07-05 20:55:44 -0400

    day 19 p1
    Little hiccup on p1 just bc i was allowing my search to sometimes overwrite a possible result with an impossible one by not returning early

---
+++ day19/main.jl
@@ -0,0 +1,47 @@
+using Memoization
+
+
+function readdata(fname)
+    data = readlines("day19/$(fname).txt")
+    towels = String.(split(data[1], ", ")) # Allocate new strings rather than SubString views
+    patterns = data[3:end]
+    return towels, patterns
+end
+
+@memoize function isPossible(pattern, towels)
+# function isPossible(pattern, towels)
+    possible = false
+    for tok in towels
+        # Token is valid, and there is more to match
+        # Do "DFS" greedy matching to accelerate through runs
+        i = 1
+        n = length(tok)
+        while i+n-1 <= length(pattern) && pattern[i:i+n-1] == tok
+            i += n
+        end
+        if i == length(pattern) + 1 # We consumed whole pattern to the end with this token
+            return true
+        elseif i > length(pattern) + 1 # We overran with the last pattern
+            println("Hit new case!")
+        elseif i > 1 # We consumed part of the pattern with this token
+            if isPossible(pattern[i:end], towels) # Always return true as soon as possible case is found
+                return true
+            end
+        end
+    end
+
+    # Fell through = could not match up whole pattern
+    return possible
+end
+
+towels, patterns = readdata("test")
+possible = [isPossible(p, towels) for p in patterns]
+for i = eachindex(possible)
+    println("$(patterns[i]) is $(possible[i] ? "possible" : "impossible")")
+end
+isPossible("bwurrg", towels)
+
+# P1
+towels, patterns = readdata("input")
+@time possible = [isPossible(p, towels) for p in patterns]
+println("$(sum(possible)) designs are possible")
